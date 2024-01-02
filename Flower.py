c90bbded3267f9e767b43eae1c18d89c6ad40a0a
nakamino24 <faiz.quways@gmail.com>
2024-01-02 20:43:55 +0700

    Flower.py

---
+++ Flower.py
@@ -0,0 +1,16 @@
+import turtle
+
+turtle.bgcolor('black')
+turtle.speed(80)
+turtle.hideturtle()
+
+colors = ["white", "salmon", "pink", "red"]
+
+for i in range(120):
+    for c in colors:
+        turtle.color(c)
+        turtle.circle(180-1, 100)
+        turtle.lt(90)
+        turtle.circle(180-1, 100)
+        turtle.end_fill()
+turtle.mainloop()
